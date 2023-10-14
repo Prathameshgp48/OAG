130f3791d83d325d5c625b57f06d12662115a579
Prathameshgp48 <120002929+Prathameshgp48@users.noreply.github.com>
2023-10-14 19:47:33 +0530

    Create ecommerce.js

---
+++ ecommerce.js
@@ -0,0 +1,15 @@
+class EcommercePortal {
+    constructor() {
+        this.artworks = [];
+    }
+
+    addArtwork(artwork) {
+        this.artworks.push(artwork);
+    }
+
+    buyArtwork(artwork, customer) {
+        // Add logic to handle the purchase of artwork by a customer.
+    }
+}
+
+module.exports = EcommercePortal;
